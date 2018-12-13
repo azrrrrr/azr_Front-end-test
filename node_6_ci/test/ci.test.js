df570b98ec8dc1facb2a99b249b87fa402334c5d
Rui <dengrui0709@gmail.com>
2018-12-13 15:09:14 +0800

    Create ci.test.js

---
+++ node_6_ci/test/ci.test.js
@@ -0,0 +1,19 @@
+const { cover } = require('../src/index.js')
+
+const assert = require("assert");
+
+describe("#index",()=>{
+  describe("#cover()",function(){
+    it("cover(3,2) equal 1", function() {
+      assert.equal(cover(3,2),1);
+    });
+    
+    it("cover(3,3) equal 3", function() {
+      assert.equal(cover(3,3),3);
+    });
+    
+    it("cover(2,4) equal 6", function() {
+      assert.equal(cover(2,4),6);
+    });
+  })
+})
